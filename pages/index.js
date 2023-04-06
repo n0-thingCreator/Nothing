39aeeaa1a98621a13834f54d02fa3645031c2b0f
n0-thingCreator <126139102+n0-thingCreator@users.noreply.github.com>
2023-04-06 09:35:07 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,6 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to Nothing!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
